8bc4ab1eea714164d0376ef02205b4998977865c
guschina.aa <v.anastasiya26@mail.ru>
2026-08-14 20:06:40 +0700

    проверка

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -2,6 +2,7 @@ export default function HomePage() {
   return (
     <main>
       <div>
+        
         <h1>Lite Notion запущен</h1>
         <p>Frontend готов к дальнейшей разработке.</p>
       </div>
